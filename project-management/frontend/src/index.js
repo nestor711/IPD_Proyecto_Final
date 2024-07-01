fca85f699e5124f293a6d2d807216effbeb2bddf
nestor711 <nestorheredia123@gmail.com>
2024-07-01 17:50:58 -0500

    Update index.js

--- project-management/frontend/src/index.js
+++ project-management/frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+// Usamos ReactDOM.createRoot para la compatibilidad con React 18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
