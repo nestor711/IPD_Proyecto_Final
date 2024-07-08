4fd3050f6ac01c24fa317a7c931cb628ca61b45a
Kevin Velez <kevin.alejandro.velez@correounivalle.edu.co>
2024-07-07 20:20:05 -0500

    Update api.js

--- project-management/frontend/src/api.js
+++ project-management/frontend/src/api.js
@@ -22,6 +22,7 @@ export const createProject = (project) => axios.post(`${API_URL}/projects`, proj
 export const deleteProject = (projectId) => axios.delete(`${API_URL}/projects/${projectId}`);
 export const fetchProjectById = (projectId) => axios.get(`${API_URL}/projects/${projectId}`);
 export const updateProject = (projectId, project) => axios.put(`${API_URL}/projects/${projectId}`, project);
+export const fetchAllProjectIds = () => axios.get(`${API_URL}/projects/all-ids`);
 
 // Funciones para tareas
 export const fetchTasks = (projectId) => axios.get(`${API_URL}/tasks`, { params: { projectId } });
@@ -29,3 +30,7 @@ export const createTask = (task) => axios.post(`${API_URL}/tasks`, task);
 export const deleteTask = (taskId) => axios.delete(`${API_URL}/tasks/${taskId}`);
 export const fetchTaskById = (taskId) => axios.get(`${API_URL}/tasks/${taskId}`);
 export const updateTask = (taskId, task) => axios.put(`${API_URL}/tasks/${taskId}`, task);
+export const fetchTasksByProjectId = (projectId) => axios.get(`${API_URL}/tasks/project/${projectId}`);
+
+
+
